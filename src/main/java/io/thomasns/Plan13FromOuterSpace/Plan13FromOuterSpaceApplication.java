82a018a8f86ba06fce662747f4f61e2e3bc2cfcd
Nathan Thomas <ki4hbd.nathan@gmail.com>
2023-02-02 19:19:59 -0500

    Initial commit, base start.spring.io starter

---
+++ src/main/java/io/thomasns/Plan13FromOuterSpace/Plan13FromOuterSpaceApplication.java
@@ -0,0 +1,13 @@
+package io.thomasns.Plan13FromOuterSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plan13FromOuterSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plan13FromOuterSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/thomasns/Plan13FromOuterSpace/Plan13FromOuterSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.thomasns.Plan13FromOuterSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plan13FromOuterSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
